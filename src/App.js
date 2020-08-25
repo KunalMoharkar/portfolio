cacee25f012a3dab563fd87f90a895375fae18fb
Kunal Moharkar <kunalmoharkar1700@gmail.com>
2020-08-25 15:46:08 +0530

    home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
+library.add(fab, faCheckSquare, faCoffee)
 
 function App() {
   return (
--- src/components/home.js
+++ src/components/home.js
@@ -1,20 +1,35 @@
 import React from 'react';
-import Avatar from '../images/imgAvatar.jpeg'
+import Avatar from '../images/imgAvatar.jpeg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {Link} from 'react-router-dom';
+import '../css/home.css';
 
 export const Home=()=>{
 
     return(
-        <div>
+        <div className="container">
             <div className="linkContainer">
-
+                <Link to={`/`}  className="links">Home</Link>
             </div>
-
             <div className="imgContainer">
                 <img className="round-img" src={Avatar}></img>
             </div>
-
+            <div className="desContainer">
+                <p>Kunal Moharkar <br/><span>Enthusiastic Learner|Fullstack Developer</span></p>
+                <a href='https://github.com/KunalMoharkar'>
+                    <FontAwesomeIcon  className="icons" icon={['fab', 'github-square']} size="2x" />
+                </a>
+                <a href='https://github.com/KunalMoharkar'>
+                    <FontAwesomeIcon  className="icons" icon={['fab', 'linkedin']} size="2x"/>
+                </a>
+                <a href='https://github.com/KunalMoharkar'>
+                    <FontAwesomeIcon  className="icons" icon={['fab', 'instagram-square']} size="2x"/>
+                </a>
+            </div>
             <div className="navContainer">
-
+                <Link to={`/About/`} className="links">About</Link>
+                <Link to={`/Skills/`} className="links">Skills</Link>
+                <Link to={`/Projects/`} className="links">Projects</Link>
             </div>
         </div>
     )
