99215ced1f4292c867216c0562d1c1a2918d3d50
Kunal Moharkar <kunalmoharkar1700@gmail.com>
2020-08-27 00:59:21 +0530

    Updates

---
+++ precache-manifest.fbaa24904754f839038c8c11d0926dd6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "287bbfbb76c8950d083e3d4963390e18",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ac9eec79f1729bd8cbde",
+    "url": "/portfolio/static/css/main.01e7198b.chunk.css"
+  },
+  {
+    "revision": "e125551f50c1c4aa3f6a",
+    "url": "/portfolio/static/js/2.8e5feeaa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.8e5feeaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac9eec79f1729bd8cbde",
+    "url": "/portfolio/static/js/main.c72255ce.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "a275485c55af3a4584ebb28eaf50a6c5",
+    "url": "/portfolio/static/media/imgAvatar.a275485c.jpeg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
