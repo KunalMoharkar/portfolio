ba9c19546a5e0be49a3d22948ec6551e09fa3d4e
Kunal Moharkar <kunalmoharkar1700@gmail.com>
2021-05-06 00:40:34 +0530

    hash router

--- src/components/routes.js
+++ src/components/routes.js
@@ -1,5 +1,5 @@
 import React, { Component} from "react";
-import { Switch, Route} from "react-router-dom";
+import { Switch, Route, HashRouter} from "react-router-dom";
 import {About} from "./about";
 import {Home} from "./home";
 import {Projects} from "./projects";
@@ -9,12 +9,12 @@ export const Routes=()=>{
 
     return(
 
-        <Switch>
+        <HashRouter>
             <Route exact path={"/"} component={Home}/>
             <Route exact path={"/About/"} component={About}/>
             <Route exact path={"/Skills/"} component={Skills}/>
             <Route exact path={"/Projects/"} component={Projects}/>
-        </Switch>
+        </HashRouter>
     )
 
 }
