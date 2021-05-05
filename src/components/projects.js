d4e91634501b4e0a1e1760a5782cfaa3a1a2f053
Kunal Moharkar <kunalmoharkar1700@gmail.com>
2021-05-05 23:45:20 +0530

    updates

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React , {useState} from 'react';
 import '../css/projects.css';
 import {Footer} from './footer';
 import {Header} from './header';
-import {ProjectList} from '../data/projects';
+import {ProjectList, ProjectList1} from '../data/projects';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 
 export const Projects=()=>{
+
+    const [more, setMore] = useState(false)
 
     return(
         
@@ -46,10 +48,57 @@ export const Projects=()=>{
                         
                       }
 
+                <div className="projectDescription" style={{'text-align':'center'}}>
+                    
+                    <p>Along with the above major projects. I have also worked on some mini projects as part of hackathons, freelancing, and most of them just because I love to do it.</p>
+                    {!more?
+                    <button className="btn btn-primary resumeButton"  onClick={() => setMore(true)}>More of my work</button>
+                    :null}
+                    </div>
+                     
+                    
+                </div>
+
+             
+                {more?
+                <div className="projectContainer">
+                    
+                    {
+
+                        ProjectList1.map((project)=>{
+
+                            return(
+                                    <div>
+                                        <div className="projectHeader">
+                                            <a href={project.links[0]}>{project.name}</a>
+                                        </div>
+                                        <div className="projectDescription">
+                                            <p>{project.description}</p>
+                                        </div>
+                                        <div className="projectFooter">     
+                                          
+                                                {
+                                                    project.tags.map((tag)=>{
+                                                        return(
+                                                            <span>
+                                                                {tag}
+                                                            </span>
+                                                        )
+                                                    })
+                                                }
+                                        </div>
+                                    </div> 
+                                )
+
+                             })
+                        
+                      }
+
 
                      
                     
                 </div>
+                :null}
 
             </div>
         </div>
--- src/data/projects.js
+++ src/data/projects.js
@@ -7,7 +7,7 @@ export const ProjectList = [
         "description":"Developed a web application that would ease the process of applying to the projects posted by faculties on the campus. Django framework was used on the backend coupled with React JS for the client side.",
         "tags":["Django","Django-REST","React","JWT"],
         "isdeployed":false,
-        "links":["https://github.com/KunalMoharkar",""]
+        "links":["https://devfolio.co/submissions/projects-corner-d4ba",""]
     },
 
     
@@ -16,7 +16,7 @@ export const ProjectList = [
         "description":"Developed a dynamic website for AXIS'21, the Technical Festival of VNIT, Nagpur. The website, hosted on AWS was responsible for all event registrations, announcements, and email notifications.",
         "tags":["Django","CSS","jQuery","Particle JS","AWS EB","AWS RDS"],
         "isdeployed":false,
-        "links":["https://github.com/KunalMoharkar",""]
+        "links":["https://devfolio.co/submissions/k-5585",""]
     },
 
     {
@@ -24,10 +24,10 @@ export const ProjectList = [
         "description":"A web application that acts as a platform to connect service professionals with the users. An admin section for managing the website was also implemented along with features like filters and dynamic loading of content. It made use of Java servlets and JSP with MYSQL as a database.",
         "tags":["Java servlets","JSP","AJAX","MYSQL","Bootstrap"],
         "isdeployed":false,
-        "links":["https://github.com/KunalMoharkar",""]
+        "links":["https://devfolio.co/submissions/proserv-2d93",""]
     },
 
-    
+/*    
     {
         "name":"Election Portal",
         "description":"A platform to conduct online elections in Universities using Django. The app followed Agile practices making use of Kanban boards and Travis CI for continuous integration. The App was also dockerized for easy development and deployment",
@@ -36,12 +36,47 @@ export const ProjectList = [
         "links":["https://github.com/KunalMoharkar",""]
     },
 
+  
+*/
+]
+
+
+
+export const ProjectList1 = [
+
+   
+
+    {
+        "name":"Rhythm",
+        "description":"A freelance project where I developed a custom website for a clothing startup.",
+        "tags":["Django", "AWS EC2", "nginx"],
+        "isdeployed":false,
+        "links":["https://rhythmsarees.in",""]
+    },
+
+    {
+        "name":"VID-SUM",
+        "description":"This project Creates a summary from YouTube videos. made use of third-party libraries for video splitting and summarization. Django Framework and AJAX were used to facilitate a user-friendly interface.",
+        "isdeployed":true,
+        "tags":["AJAX","Django"],
+        "links":["https://devfolio.co/submissions/vid-sum-7ac9",""]
+    },
+
+    {
+        "name":"Heap Sim",
+        "description":"Developed a web app simulating the Heap Sort algorithm using React JS on ASCII Array.",
+        "tags":["React"],
+        "isdeployed":true,
+        "links":["https://github.com/KunalMoharkar/Heapsim",""]
+    },
+
+
     {
         "name":"TODO APP",
         "description":"A simple todo app made using the LAMP stack. Users can Add/Modify/Delete the todos. Functionality to mark todo as favorite/completed was also implemented.",
         "tags":["PHP", "CSS", "MYSQL"],
         "isdeployed":false,
-        "links":["https://github.com/KunalMoharkar",""]
+        "links":["https://github.com/KunalMoharkar/TODO-APP",""]
     },
 
     {
@@ -49,8 +84,24 @@ export const ProjectList = [
         "description":"A static React Native mobile app is a collection of trending technologies and useful resources for programmers. A quotes section using remote API calls was also implemented. ",
         "tags":["React-Native","Expo","React-navigation"],
         "isdeployed":true,
-        "links":["https://github.com/KunalMoharkar",""]
+        "links":["https://github.com/KunalMoharkar/compkit",""]
     },
 
-   
+    {
+        "name":"Algo-sim",
+        "description":"This project is an attempt to simulate common CS algorithms for their better understanding. It makes use of React JS.",
+        "tags":["React"],
+        "isdeployed":true,
+        "links":["https://github.com/KunalMoharkar/Algo-sim",""]
+    },
+
+
+    {
+        "name":"Pomodoro Timer",
+        "description":"React Native timer application based on the famous Pomodoro technique.",
+        "tags":["React-Native","Expo"],
+        "isdeployed":true,
+        "links":["https://github.com/KunalMoharkar/PomodoroTimer",""]
+    },
+
 ]
